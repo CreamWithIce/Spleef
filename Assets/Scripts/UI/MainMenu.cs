b423dfdc7eb37e6fec72d601a791f8734b948ed5
matt <mcrd2407@gmail.com>
2022-06-08 11:28:09 +1000

    (Probably) final version

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using TMPro;
 public class MainMenu : MonoBehaviour
 {
+
+    public TMP_Text versionTxt;
     public void play(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     public void quit(){
         Application.Quit();
+    }
+
+    void Start(){
+        versionTxt.text = Application.version;
+
     }
 }
